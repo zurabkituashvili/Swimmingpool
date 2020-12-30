01e4c23765075379eecb228ee9991109dfcca1a2
seidl <seidl@in.tum.de>
2020-12-30 14:08:20 +0100

    ...

--- src/eidi/Restaurant.java
+++ src/eidi/Restaurant.java
@@ -5,7 +5,6 @@ import java.util.*;
 public class Restaurant {
 	private GuestWaitingQueue tasks = null;
 	private Table[] threads = null;
-	// private List<Table> threads = new ArrayList<>();
 
 	public Restaurant(int threadCount){
         // TODO
